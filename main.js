47489b130e38f16b117c7e0f9f5f5908aea26d6f
rjrfrst <rjrfrst@gmail.com>
2024-03-14 21:22:56 +0000

    links in the nav bar are now function (I think, need to test it out further) meaning that when link click nav bar closes. Working on the main section

--- main.js
+++ main.js
@@ -20,3 +20,17 @@ if(navMenuClose){
         navMenu.classList.remove('show-menu'); 
     })
 }
+
+// Now I want to close the nav bar when EACH link is clicked
+// Get all the nav link
+const navLink = document.querySelectorAll('.nav-link')
+// Create an arrow function that will do the following:
+// Get the nav menu
+// when I get the nav menu remove the show-menu
+// Maybe a forloop or for each when the links are clicked I want to repeat the removal of show menu
+
+const navLinkListAction = () => {
+    const navMenu = document.querySelector('.nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(linkedItem => linkedItem.addEventListener('click', navLinkListAction));
